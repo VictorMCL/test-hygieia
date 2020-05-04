6a6f1bbde47f58b9487604f12cc67fe31039b061
Victor Capote <48441259+VictorMCL@users.noreply.github.com>
2020-05-03 22:09:33 -0500

    segundo prueba

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -4,7 +4,7 @@ def test_sumar():
     assert sumar(3,2) == '5' 
 
  def test_restar():
-#     assert restar(3,2) == '1' 
+     assert restar(3,2) == '1' 
     
 # def test_multiplicar():
 #     assert multiplicar(2,3) == '6' 
